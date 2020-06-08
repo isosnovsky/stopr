d63b26405c45de8d345e0db377b972dcfbf3e887
Ilya Sosnovsky <highfive.bear@gmail.com>
2020-06-09 00:58:45 +0400

    webpack:asset management part

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import './style.css';
+
 function component() {
     const element = document.createElement('div');
-
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = 'Атятя'
+    element.innerHTML = 'Атятя';
+    element.classList.add('hello');
 
     return element;
 }
